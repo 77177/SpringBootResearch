6f91bbbe15c26959a407179834e1eb0000f475ac
Stanislav_Tarasov <iluvstrwrs@mail.ru>
2019-05-06 15:57:26 +0300

    added databse auth

---
+++ src/main/java/com/epam/model/User.java
@@ -0,0 +1,17 @@
+package com.epam.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+    private boolean enabled;
+}
