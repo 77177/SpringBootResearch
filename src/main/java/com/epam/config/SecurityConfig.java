c4248dd4bdcd047513171bde11ed2ffbbdd75733
Stanislav_Tarasov <iluvstrwrs@mail.ru>
2019-05-13 23:29:05 +0300

    security config changed

--- src/main/java/com/epam/config/SecurityConfig.java
+++ src/main/java/com/epam/config/SecurityConfig.java
@@ -1,17 +1,13 @@
 package com.epam.config;
 
-import com.epam.filters.MyFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.sql.DataSource;
